5b8a9d86d6d0c880f0ad66e1da56ef4cb59e1046
AB <anitabanhtx@gmail.com>
2023-02-08 10:02:02 -0600

    remove individual contributor employee function

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ function mainQuestions() {
           "add a department",
           "add a role",
           "add an employee",
-          "update an employee role",
+          // "update an employee role",
         ],
       },
     ])
@@ -64,9 +64,9 @@ function mainQuestions() {
         case "add an employee":
           addEmployee();
           break;
-        case "update an employee role":
-          updateEmployeeRole();
-          break;
+        // case "update an employee role":
+        //   updateEmployeeRole();
+        //   break;
       }
     });
 }
@@ -169,133 +169,131 @@ function addRole() {
       });
     })
   })
-}
-
-// HOW DO I LIMIT ROLES LIST TO MANAGER_ID NULL OR NON NULL? SELECT * FROM employees_db.employee WHERE employee.manager_id IS NULL" in workbench shows employees where manager_id is null. Join table? "SELECT * FROM employees_db.role" shows all roles. need columns from role: title, id and from employee: 
-// prompt "Is this employee a people manager?"
-  // if yes, new functions to add ppl manger INSERT INTO employee (first_name, last_name, role_id) VALUES (?,??,???). present roleid as list of existing titles.
-  // if no, new function individual contributor INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ??, ???, ????)
-function addEmployee() {
-  db.promise().query("SELECT * FROM employees_db.role")
-  .then( ([rows,fields]) => {
-    var roleChoices = rows.map(({id, title}) => {
-      return {
-        name: title,
-        value: id
-      };
-    });
-    db.promise().query("SELECT * FROM employees_db.employee")
-    .then( ([rows,fields]) => { 
-    var managerChoices = rows.map(({id, first_name, last_name}) => {
-      return {
-        name: `${first_name} ${last_name}`,
-        value: id
-      };
-    });
-    
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'first_name',
-        message: "What is the FIRST name of the employee to add?",
-        validate: (answer) => {
-          if (answer !== '') {
-            return true;
-          }
-          return 'Please enter at least one character.';
-        },
-      },
-      {
-        type: 'input',
-        name: 'last_name',
-        message: "What is the LAST name of the employee to add?",
-        validate: (answer) => {
-          if (answer !== '') {
-            return true;
-          }
-          return 'Please enter at least one character.';
-        },
-      },
-      {
-        type: 'list',
-        name: 'manager_id',
-        message: "Who is the employee's manager?",
-        choices: managerChoices
-      },
-      {
-        type: 'list',
-        name: 'role_id',
-        message: "What is the employee's role?",
-        choices: roleChoices
-      },
-
-    ])
-    .then((data) => {
-      db.query(`INSERT INTO employee SET ?`, data, (err,result) => {
-        if (err) {
-          console.log(err);
-        }
-        viewAllEmployees();
-      });
-    })})
-  })
 };
 
-function addEmployeeIndividual() {
-  db.promise().query("SELECT * FROM employees_db.role ")
-  .then( ([rows,fields]) => {
-    var choices = rows.map(({id, title}) => {
-      // console.table(choices);
-      return {
-        name: title,
-        value: id
-      };
-     
-    });
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'first_name',
-        message: "What is the FIRST name of the employee to add?",
-        validate: (answer) => {
-          if (answer !== '') {
-            return true;
-          }
-          return 'Please enter at least one character.';
-        },
-      },
-      {
-        type: 'input',
-        name: 'last_name',
-        message: "What is the LAST name of the employee to add?",
-        validate: (answer) => {
-          if (answer !== '') {
-            return true;
-          }
-          return 'Please enter at least one character.';
-        },
-      },
-      {
-        type: 'list',
-        name: 'role_id',
-        message: "What is the title for this employee?",
-        choices
-      }
-    ])
-    .then((data) => {
-      db.query(`INSERT INTO employee SET ?`, data, (err,result) => {
-        if (err) {
-          console.log(err);
-        }
-        viewAllEmployees();
-      });
-    })
-  })
-}
+// function addEmployee() {
+//   db.promise().query("SELECT * FROM employees_db.role")
+//   .then( ([rows,fields]) => {
+//     var roleChoices = rows.map(({id, title}) => {
+//       return {
+//         name: title,
+//         value: id
+//       };
+//     });
+//     db.promise().query("SELECT * FROM employees_db.employee")
+//     .then( ([rows,fields]) => { 
+//     var managerChoices = rows.map(({id, first_name, last_name}) => {
+//       return {
+//         name: `${first_name} ${last_name}`,
+//         value: id
+//       };
+//     });
+    
+//     inquirer.prompt([
+//       {
+//         type: 'input',
+//         name: 'first_name',
+//         message: "What is the FIRST name of the employee to add?",
+//         validate: (answer) => {
+//           if (answer !== '') {
+//             return true;
+//           }
+//           return 'Please enter at least one character.';
+//         },
+//       },
+//       {
+//         type: 'input',
+//         name: 'last_name',
+//         message: "What is the LAST name of the employee to add?",
+//         validate: (answer) => {
+//           if (answer !== '') {
+//             return true;
+//           }
+//           return 'Please enter at least one character.';
+//         },
+//       },
+//       {
+//         type: 'list',
+//         name: 'manager_id',
+//         message: "Who is the employee's manager?",
+//         choices: managerChoices
+//       },
+//       {
+//         type: 'list',
+//         name: 'role_id',
+//         message: "What is the employee's role?",
+//         choices: roleChoices
+//       },
+
+//     ])
+//     .then((data) => {
+//       db.query(`INSERT INTO employee SET ?`, data, (err,result) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         viewAllEmployees();
+//       });
+//     })})
+//   })
+// };
+
+
 app.use((req, res) => {
   res.status(404).end();
 });
 
+// --  update an employee role
+// UPDATE employee SET role_id = ? WHERE id = ?;
+
+// function updateEmployeeRole() {
+//   db.promise().query("SELECT * FROM employees_db.department")
+//   .then( ([rows,fields]) => {
+//     var choices = rows.map(({id, deptName}) => {
+//       return {
+//         name: deptName,
+//         value: id
+//       }
+//     });
+//     inquirer.prompt([
+//       {
+//         type: 'input',
+//         name: 'title',
+//         message: "What is the title of the role to add?",
+//         validate: (answer) => {
+//           if (answer !== '') {
+//             return true;
+//           }
+//           return 'Please enter at least one character.';
+//         },
+//       },
+//       {
+//         type: 'input',
+//         name: 'salary',
+//         message: "What is the salary of this role?",
+//         validate: (answer) => {
+//           if (answer !== '') {
+//             return true;
+//           }
+//           return 'Please enter at least one number.';
+//         },
+//       },
+//       {
+//         type: 'list',
+//         name: 'department_id',
+//         message: "What department does this role belong to?",
+//         choices
+//       }
+//     ])
+//     .then((data) => {
+//       db.query(`INSERT INTO role SET ?`, data, (err,result) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         viewAllRoles();
+//       });
+//     })
+//   })
+// };
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
